1ba479407efb78f23d6230aa7bc650dec4b84611
Inei1 <neilrmoon@gmail.com>
2025-02-22 16:38:11 -0800

    comment out invalid code

--- Settlements/TradeUI.cs
+++ Settlements/TradeUI.cs
@@ -248,7 +248,7 @@ public partial class TradeUI : Control
 
         PackedScene shopItemsScene = GD.Load<PackedScene>("res://Settlements/trade.tscn");
         foreach (ItemListing item in GetItemListings()) {
-            RichTextLabel itemName = shopItemsScene.Instantiate();
+            //RichTextLabel itemName = shopItemsScene.Instantiate();
         }
         GetNode<VBoxContainer>("VBoxContainer");
 
